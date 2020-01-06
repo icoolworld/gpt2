cdd8d28362bab738fc3a47145335bf79679bead3
coolbaby <coolbaby>
2020-01-06 14:59:52 +0800

    reduce batch size

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,10 @@
 python3 train.py \
-    --model_config config/model_config_small.json \
+    --model_config config/model_config.json \
     --tokenized_data_path data/tokenized/ \
-    --tokenizer_path cache/vocab_small.txt \
+    --tokenizer_path cache/vocab_all.txt \
     --raw_data_path data/train.json \
     --epochs 30 \
+    --batch_size 2 \
     --log_step 200 \
     --stride 512 \
     --output_dir model/ \
